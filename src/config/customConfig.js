521a67cd8d824899cc1b39fd0b9a0110cf3ba7ff
ionut <ionut.cioarec@yahoo.com>
2023-05-13 11:43:52 +0300

    Added new config parameters for v2 stake

--- src/config/customConfig.js
+++ src/config/customConfig.js
@@ -1,4 +1,4 @@
-export const networkId = "mainnet";
+export const networkId = "devnet";
 
 export const customConfig = {
     mainnet: {
@@ -9,7 +9,9 @@ export const customConfig = {
         stakeAddress: "erd1qqqqqqqqqqqqqpgql0yx4uetca8g4wmr96d9z7094vj3uhpt4d6qm5srfk",
         bloodshedAddress: "erd1qqqqqqqqqqqqqpgq47jpmgd7z3v036fqyzt5nhajzau8ygxryl5stecvv3",
         bloodshedXLHAddress: "erd1qqqqqqqqqqqqqpgq7u7e4lggx0vp09wyklu4cjh233nfchtayl5sq0xp43",
-        bloodshedToken: "VEGLD-2b9319"
+        bloodshedToken: "VEGLD-2b9319",
+        stakeV2Address: "",
+        stakeV2SFT: ""
     },
 
     devnet: {
@@ -20,7 +22,9 @@ export const customConfig = {
         stakeAddress: "erd1qqqqqqqqqqqqqpgqxmeg3k0ty84hm3f8n9wdfpukspc0asj3pa7qtt6j0t",
         bloodshedAddress: "erd1qqqqqqqqqqqqqpgq4m4udte9wj2exl3n08vu4wln87l4kpvp6ppspedm6t",
         bloodshedXLHAddress: "",
-        bloodshedToken: "WEB-5d08be"
+        bloodshedToken: "WEB-5d08be",
+        stakeV2Address: "erd1qqqqqqqqqqqqqpgqwhqpykc0p9z08qgd85echw9jm4lv66nnpa7qrqyj9c",
+        stakeV2SFT: "SFT-8ff335"
     },
 
     testnet: {
@@ -31,7 +35,9 @@ export const customConfig = {
         stakeAddress: "erd1qqqqqqqqqqqqqpgqdw9gatcwzlsdtvjwu3mveln57g0quyzzpa7q9jg84s",
         bloodshedAddress: "",
         bloodshedXLHAddress: "",
-        bloodshedToken: ""
+        bloodshedToken: "",
+        stakeV2Address: "",
+        stakeV2SFT: ""
     }
 };
 
