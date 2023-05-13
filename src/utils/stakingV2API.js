a74ca855bba085ea36cd6d17fc867827cdabd248
ionut <ionut.cioarec@yahoo.com>
2023-05-13 11:48:00 +0300

    Created stakeXLH, claimXLH, unstakeXLH and stakeSFT methods for staking v2

---
+++ src/utils/stakingV2API.js
@@ -0,0 +1,164 @@
+import {
+    AbiRegistry,
+    Address, BigUIntValue, ContractFunction,
+    SmartContract,
+    SmartContractAbi,
+    TokenPayment,
+    TransactionPayload, U64Value,
+    U8Value
+} from "@multiversx/sdk-core/out";
+import {refreshAccount} from "@multiversx/sdk-dapp/__commonjs/utils";
+import {sendTransactions} from "@multiversx/sdk-dapp/services";
+import {BigNumber} from "bignumber.js";
+import {multiplier} from "utils/utilities";
+
+export const stake = async (abiFile, scAddress, scName, chainID, token, amount, pool) => {
+    try {
+        let abiRegistry = AbiRegistry.create(abiFile);
+        let abi = new SmartContractAbi(abiRegistry, [scName]);
+        let contract = new SmartContract({
+            address: new Address(scAddress),
+            abi: abi
+        });
+
+        const transaction = contract.methodsExplicit
+            .stakeXlh([new U64Value(pool)])
+            .withChainID(chainID)
+            .withSingleESDTTransfer(
+                TokenPayment.fungibleFromAmount(token, amount, 18)
+            )
+            .buildTransaction();
+        const stakeTransaction = {
+            value: 0,
+            data: Buffer.from(transaction.getData().valueOf()),
+            receiver: scAddress,
+            gasLimit: '15000000'
+        };
+        await refreshAccount();
+
+        const { sessionId /*, error*/ } = await sendTransactions({
+            transactions: stakeTransaction,
+            transactionsDisplayInfo: {
+                processingMessage: 'Processing Stake transaction',
+                errorMessage: 'An error has occurred during Stake transaction',
+                successMessage: 'Stake transaction successful'
+            },
+            redirectAfterSign: false
+        });
+
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const unstake = async (abiFile, scAddress, scName, chainID, token, amount, pool) => {
+    try {
+        let abiRegistry = AbiRegistry.create(abiFile);
+        let abi = new SmartContractAbi(abiRegistry, [scName]);
+        let contract = new SmartContract({
+            address: new Address(scAddress),
+            abi: abi
+        });
+
+        const transaction = contract.methodsExplicit
+          .unstakeXlh([new U64Value(pool), new BigUIntValue(new BigNumber(amount * multiplier))])
+          .withChainID(chainID)
+          .buildTransaction();
+        const stakeTransaction = {
+            value: 0,
+            data: Buffer.from(transaction.getData().valueOf()),
+            receiver: scAddress,
+            gasLimit: '15000000'
+        };
+        await refreshAccount();
+
+        const { sessionId /*, error*/ } = await sendTransactions({
+            transactions: stakeTransaction,
+            transactionsDisplayInfo: {
+                processingMessage: 'Processing Unstake transaction',
+                errorMessage: 'An error has occurred during Unstake transaction',
+                successMessage: 'Unstake transaction successful'
+            },
+            redirectAfterSign: false
+        });
+
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+
+export const claim = async (abiFile, scAddress, scName, chainID, pool) => {
+    try {
+        let abiRegistry = AbiRegistry.create(abiFile);
+        let abi = new SmartContractAbi(abiRegistry, [scName]);
+        let contract = new SmartContract({
+            address: new Address(scAddress),
+            abi: abi
+        });
+
+        const transaction = contract.methodsExplicit
+          .claimRewards([new U64Value(pool)])
+          .withChainID(chainID)
+          .buildTransaction();
+        const stakeTransaction = {
+            value: 0,
+            data: Buffer.from(transaction.getData().valueOf()),
+            receiver: scAddress,
+            gasLimit: '15000000'
+        };
+        await refreshAccount();
+
+        const { sessionId /*, error*/ } = await sendTransactions({
+            transactions: stakeTransaction,
+            transactionsDisplayInfo: {
+                processingMessage: 'Processing Claim transaction',
+                errorMessage: 'An error has occurred during claim transaction',
+                successMessage: 'Claim transaction successful'
+            },
+            redirectAfterSign: false
+        });
+
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const stakeSFT = async (abiFile, scAddress, scName, chainID, token) => {
+    try {
+        let abiRegistry = AbiRegistry.create(abiFile);
+        let abi = new SmartContractAbi(abiRegistry, [scName]);
+        let contract = new SmartContract({
+            address: new Address(scAddress),
+            abi: abi
+        });
+
+        const transaction = contract.methodsExplicit
+          .stakeSft()
+          .withChainID(chainID)
+          .withSingleESDTTransfer(
+            TokenPayment.semiFungible(token, 1, 1)
+          )
+          .buildTransaction();
+        const stakeSFTTransaction = {
+            value: 0,
+            data: Buffer.from(transaction.getData().valueOf()),
+            receiver: scAddress,
+            gasLimit: '15000000'
+        };
+        await refreshAccount();
+
+        const { sessionId /*, error*/ } = await sendTransactions({
+            transactions: stakeSFTTransaction,
+            transactionsDisplayInfo: {
+                processingMessage: 'Processing SFT Stake transaction',
+                errorMessage: 'An error has occurred during SFT Stake transaction',
+                successMessage: 'SFT Stake transaction successful'
+            },
+            redirectAfterSign: false
+        });
+
+    } catch (error) {
+        console.error(error);
+    }
+};
