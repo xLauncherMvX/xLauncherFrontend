635229ed437641d8cbc0eb8b59733e43373ac4a6
ionut <ionut.cioarec@yahoo.com>
2023-05-20 19:23:20 +0300

    Fixed the intlNumberFormat function

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -62,19 +62,21 @@ export function calculateGasLimit(clientStateData) {
 }
 
 //Format number
-export function intlNumberFormat(number, locales, minDigits, maxDigits){
-    return new Intl.NumberFormat(locales? locales : "en-GB", {
-        minimumFractionDigits: minDigits? minDigits : 2,
-        maximumFractionDigits: maxDigits? maxDigits : 2,
-    }).format(number);
+export function intlNumberFormat(number, locales, minDigits, maxDigits) {
+    const options = {
+        minimumFractionDigits: minDigits !== undefined ? minDigits : 2,
+        maximumFractionDigits: maxDigits !== undefined ? maxDigits : 2,
+    };
+
+    return new Intl.NumberFormat(locales ? locales : "en-GB", options).format(number);
 }
 
 export const openInNewTab = (url) => {
     const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
     if (newWindow) newWindow.opener = null;
-}
+};
 
 export const openInSameTab = (url) => {
     const newWindow = window.open(url, '_self', 'noopener,noreferrer')
     if (newWindow) newWindow.opener = null;
-}
+};
