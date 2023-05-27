dd449006056906f04ed9320ac15f966799e191d2
ionut <ionut.cioarec@yahoo.com>
2023-05-27 15:25:30 +0300

    Added social links to sidenav

--- src/config/customConfig.js
+++ src/config/customConfig.js
@@ -2,6 +2,7 @@ export const networkId = "devnet";
 
 export const customConfig = {
     mainnet: {
+        website: 'https://app.x-launcher.com/',
         apiAddress: 'https://api.multiversx.com',
         provider: 'https://gateway.multiversx.com',
         apiLink: 'https://api.multiversx.com/accounts/',
@@ -15,6 +16,7 @@ export const customConfig = {
     },
 
     devnet: {
+        website: 'https://devnet-app.x-launcher.com/',
         apiAddress: 'https://devnet-api.multiversx.com',
         provider: 'https://devnet-gateway.multiversx.com',
         apiLink: 'https://devnet-api.multiversx.com/accounts/',
@@ -28,6 +30,7 @@ export const customConfig = {
     },
 
     testnet: {
+        website: 'https://testnet-app.x-launcher.com/',
         apiAddress: 'https://testnet-api.multiversx.com',
         provider: 'https://testnet-gateway.multiversx.com',
         apiLink: 'https://testnet-api.multiversx.com/accounts/',
--- src/sidenav/components/sidenavHeader.js
+++ src/sidenav/components/sidenavHeader.js
@@ -1,31 +1,51 @@
-import { ReactComponent as XLauncherLogo } from "assets/images/logo.svg";
-import { useProSidebar } from "react-pro-sidebar";
+import {ReactComponent as XLauncherLogo} from "assets/images/logo.svg";
+import {useProSidebar} from "react-pro-sidebar";
 import 'assets/css/sidenavHeader.css';
 import 'assets/css/globals.css';
 import React from "react";
 import Button from "react-bootstrap/Button";
-import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {openInNewTab} from "utils/utilities";
+import WebsiteIcon from '@mui/icons-material/Public';
+import IconButton from "@mui/material/IconButton";
+import {networkId, customConfig} from "config/customConfig";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import TelegramIcon from '@mui/icons-material/Telegram';
+
 
 export const SidenavHeader = () => {
-    const { toggleSidebar, broken } = useProSidebar();
-    return (
-        <React.Fragment>
-            <div className='styled-sidebar-header'>
-                <div className='styled-sidebar-header-div'>
-                    <XLauncherLogo className="logo"/>
-                    <p className='logo-text'>
-                        XLauncher
-                    </p>
-                </div>
-                {broken ? (
-                    <Button variant="link" size="sm" style={{width: '40px', marginLeft: '-20px'}} onClick={() => toggleSidebar()}>
-                        <FontAwesomeIcon icon="fa-circle-xmark" />
-                    </Button>
-                ):(
-                    ''
-                )}
-            </div>
-            <div className='light-divider'> </div>
-        </React.Fragment>
-    );
+	const config = customConfig[networkId];
+	const {toggleSidebar, broken} = useProSidebar();
+
+	return (
+		<React.Fragment>
+			<div className='styled-sidebar-header'>
+				<div className='styled-sidebar-header-div'>
+					<XLauncherLogo className="logo"/>
+					<p className='logo-text'>
+						XLauncher
+					</p>
+				</div>
+				{broken ? (
+					<Button variant="link" size="sm" style={{width: '40px', marginLeft: '-20px'}} onClick={() => toggleSidebar()}>
+						<FontAwesomeIcon icon="fa-circle-xmark"/>
+					</Button>
+				) : (
+					''
+				)}
+			</div>
+			<div className='light-divider' style={{marginBottom: '1px'}}></div>
+			<div className="d-flex justify-content-center">
+				<IconButton className="text-white" onClick={() => openInNewTab(config.website)}>
+					<WebsiteIcon fontSize="small"/>
+				</IconButton>
+				<IconButton className="float-right text-white" onClick={() => openInNewTab("https://twitter.com/XLauncher_")}>
+					<TwitterIcon fontSize="small"/>
+				</IconButton>
+				<IconButton className="float-right text-white" onClick={() => openInNewTab("https://t.me/XLauncherChat")}>
+					<TelegramIcon fontSize="small"/>
+				</IconButton>
+			</div>
+		</React.Fragment>
+	);
 };
