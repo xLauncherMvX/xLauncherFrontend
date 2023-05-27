d4f7070a67e084142c6336a4f8756af5c9a143e9
ionut <ionut.cioarec@yahoo.com>
2023-05-27 15:26:38 +0300

    Added whitepaper_ro link in sidenav footer

--- src/sidenav/components/sidenavFooter.js
+++ src/sidenav/components/sidenavFooter.js
@@ -3,26 +3,29 @@ import 'assets/css/sidenavFooter.css';
 import 'assets/css/globals.css';
 import React from "react";
 
-const codeUrl =
-    'https://x-launcher.com/files/Whitepaper.pdf';
+const codeUrl = 'https://x-launcher.com/files/Whitepaper.pdf';
+const codeUrl_ro = 'https://x-launcher.com/files/Whitepaper_ro.pdf';
 
 export const SidenavFooter = () => {
-    return (
-        <div className='sidenav-container'>
-            <div>
-                <div className='star-container'>
-                    <Star className="star-logo"/>
-                </div>
-                <div className='card-container'>
-                    <p className="h6">Need more info?</p>
-                    <p style={{fontSize: '13px' , marginTop: '-7px'}}>Check our whitepaper</p>
-                </div>
-                <div className='d-grid gap-2 button-container' >
-                    <a href={codeUrl} target='_blank' rel="noreferrer" className='btn btn-gradient-dark btn-sm'>
-                        View file
-                    </a>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className='sidenav-container'>
+			<div>
+				<div className='star-container'>
+					<Star className="star-logo"/>
+				</div>
+				<div className='card-container'>
+					<p className="h6">Need more info?</p>
+
+				</div>
+				<div className='d-grid gap-2 button-container'>
+					<a href={codeUrl} target='_blank' rel="noreferrer" className='btn btn-gradient-dark btn-sm'>
+						Whitepaper EN
+					</a>
+					<a href={codeUrl_ro} target='_blank' rel="noreferrer" className='btn btn-gradient-dark btn-sm'>
+						Whitepaper RO
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 };
