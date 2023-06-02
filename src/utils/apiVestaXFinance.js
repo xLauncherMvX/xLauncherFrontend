617413694ad1bd3a787595490727c681016a8ff9
Ionut <35095884+IonutCioarec@users.noreply.github.com>
2023-06-02 13:41:05 +0300

    Updated apiVestaXFinance.js based on the new mvx modules version

--- src/utils/apiVestaXFinance.js
+++ src/utils/apiVestaXFinance.js
@@ -1,22 +1,20 @@
 import { multiplier } from "utils/utilities";
-import {TransactionPayload} from "@multiversx/sdk-core/out/transactionPayload";
-import {BigUIntValue, ContractFunction, U32Value} from "@multiversx/sdk-core/out";
-import {BytesValue} from "@multiversx/sdk-core/out/smartcontracts/typesystem/bytes";
+import {U32Value} from "@multiversx/sdk-core/out";
 import {BigNumber} from "bignumber.js";
 import { refreshAccount } from "@multiversx/sdk-dapp/utils/account";
 import { sendTransactions} from "@multiversx/sdk-dapp/services";
-import { contractQuery} from "utils/api";
+import {ArgSerializer} from "@multiversx/sdk-core/out/smartcontracts/argSerializer";
 
 //Mint Function
 export const mintFunction = async (mintAmount, mintAddress, setTransactionSession) => {
     console.log("Formatting mint transaction");
 
-    let data = TransactionPayload.contractCall()
-        .setFunction(new ContractFunction("mint"))
-        .setArgs([
-            new U32Value(mintAmount),
-        ])
-        .build().toString();
+    const args = [
+        new U32Value(mintAmount),
+    ];
+
+    const { argumentsString } = new ArgSerializer().valuesToString(args);
+    const data = `mint@${argumentsString}`;
 
     const createMintTransaction = {
         value: new BigNumber(0.85 * multiplier).multipliedBy(mintAmount).toFixed(),
@@ -31,7 +29,7 @@ export const mintFunction = async (mintAmount, mintAddress, setTransactionSessio
         transactions: [createMintTransaction],
         transactionsDisplayInfo: {
             processingMessage: "Mint Transaction",
-            errorMessage: "An error has occured during Mint Transaction",
+            errorMessage: "An error has occurred during Mint Transaction",
             successMessage: "Mint Transaction successful",
         },
         redirectAfterSign: false,
