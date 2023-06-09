8ab08aea5f5670815eddf9398c952b55d109c4fd
ionut <ionut.cioarec@yahoo.com>
2023-06-10 02:20:23 +0300

    Resolved some conflicts

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ import BloodshedReveal from "pages/presale/bloodshed_reveal";
 import Zero2Infinity from "pages/projects/Zero2Infinity";
 import EstarGames from "pages/projects/EstarGames";
 import VestaXFinance from "pages/projects/VestaXFinance";
+import { Presale } from "pages/snake_presale/Presale";
 import { DappProvider } from "@multiversx/sdk-dapp/wrappers/DappProvider";
 import {
   NotificationModal,
@@ -124,6 +125,7 @@ function App() {
         { path: "/projects/estar-games", element: <EstarGames /> },
         { path: "/projects/vestax-finance", element: <VestaXFinance /> },
         { path: "/staking2.0", element: <StakingV2 walletState={walletState} /> },
+        { path: "/snake-token-presale", element: <Presale /> },
 
         { path: "*", element: <Dashboard /> },
       ],
