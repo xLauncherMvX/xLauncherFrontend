574a960b43b0c9825af64b4d83cfc3b1ea533a9a
Ionut <35095884+IonutCioarec@users.noreply.github.com>
2023-09-26 12:36:59 +0300

    Created a new reusable component: countdown

---
+++ src/components/countdown.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "assets/css/mint.css";
+import Countdown from 'react-countdown';
+
+export default function CustomCountdown({ startTitle, completedTitle, startTimestamp }) {
+
+	// build the countdown timer
+	const countdownRenderer = ({ days, hours, minutes, seconds, completed, }) => {
+		if (completed) {
+			return (
+				<>
+					<div className="presale-timer-container text-center mt-4">
+						{completedTitle && (<p className="mb-4 h1">{completedTitle}</p>)}
+					</div>
+				</>
+			);
+		} else {
+			return (
+				<div className="presale-timer-container text-center mt-4">
+					{startTitle && (<p className="mb-4 h1">{startTitle}</p>)}
+					<strong>
+						<span className="presale-timer-box p-2 me-2">{String(days).padStart(2, '0')} D</span>
+						<span>: </span>
+						<span className="presale-timer-box p-2 me-2">{String(hours).padStart(2, '0')} h</span>
+						<span>: </span>
+						<span className="presale-timer-box p-2 me-2">{String(minutes).padStart(2, '0')} m</span>
+						<span>: </span>
+						<span className="presale-timer-box p-2 me-2">{String(seconds).padStart(2, '0')} s</span>
+					</strong>
+				</div>
+			);
+		}
+	};
+
+	return (
+		<Countdown renderer={countdownRenderer} date={startTimestamp} autoStart/>
+	);
+}
