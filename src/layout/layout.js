2a0b5ff12459b49eea09401d44e68646c905b995
ionut <ionut.cioarec@yahoo.com>
2023-11-13 22:29:03 +0200

    Added ouro balance to midas chain lottery page

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Outlet } from "react-router-dom";
 import { Sidenav } from "sidenav/sidenav";
 import { Navbar } from "navbar/navbar";
 import { Footer } from "footer/footer";
 import "assets/css/globals.css";
+import "assets/css/starWars.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
